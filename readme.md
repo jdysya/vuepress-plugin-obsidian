4a96f0980d67a0f1b73267844f9922c65164f9d9
xiangyu <xyufj@foxmail.com>
2023-06-03 21:16:48 +0800

    增加tabs特殊情况处理

--- readme.md
+++ readme.md
@@ -110,7 +110,7 @@ export default defineUserConfig({
 
 ## 五、选项卡tabs的支持
 
-支持了`tabs`增强语法,在`obsidian`对应的插件是[obsidian-code-tab](https://github.com/lazyloong/obsidian-code-tab),为了语法上的统一,这里本人将其源码进行了小小的修改,其原本的语法是`tab`,修改为`tabs`,然后本插件就会在编译时将对应的语法转换为`vuepress`中的语法
+支持了`tabs`增强语法,在`obsidian`对应的插件是[obsidian-code-tab](https://github.com/lazyloong/obsidian-code-tab),为了语法上的统一,这里本人将其源码进行了小小的修改,其原本的语法是`tab`,修改为`tabs`,然后本插件就会在编译时将对应的语法转换为`vuepress`中的语法,需要注意的是,若在`tabs`中嵌套其他的容器(例如代码块这种包括反引号的块级内容),此时`tabs`需要使用四个反引号,平时可以直接使用三个反引号
 
 - `ob`中的效果
 
--- src/markdown-it-plugins/tabsReplace.ts
+++ src/markdown-it-plugins/tabsReplace.ts
@@ -5,9 +5,18 @@ export function tabsReplace(md) {
     // Replace the fence blocks with container syntax before parsing
     md.core.ruler.before('normalize', 'custom-fence', state => {
         state.src = state.src.replace(fenceRegex, match => {
-            match = match.replaceAll('````', '::::')
-                .replaceAll('tab:', '@tab ')
-            return match;
+            // 在这里处理匹配到的围栏块
+            // 要求容器内嵌套容器时,需要使用四个反引号
+            if (match.indexOf('````') !== -1) {
+                // 说明当前使用的是三个反引号
+                match = match.replaceAll('````', '::::')
+            }
+            else {
+                match = match.replaceAll('```', ':::')
+            }
+            match = match.replaceAll('tab:', '@tab ')
+            // 可以根据需求对围栏块的内容进行修改或替换
+            return match; // 返回处理后的围栏块内容
         });
     });
 }
